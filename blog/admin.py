9a120690bafc96da8b4572a890c3c4af438be09a
danielpriestley <dannypriestley3@gmail.com>
2019-03-19 10:14:42 +0000

    removed coaches and staff

--- blog/admin.py
+++ blog/admin.py
@@ -3,11 +3,9 @@ from .models import Post
 from .models import Profile
 from .models import Event
 from .models import GalleryPhoto
-from .models import Staff
 
 
 admin.site.register(Post)
 admin.site.register(Profile)
 admin.site.register(Event)
 admin.site.register(GalleryPhoto)
-admin.site.register(Staff)
